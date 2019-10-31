6477597c6ae52688c850dc49e51c30f4b44f2a59
nafees iqbal <iqbalnafee@gmail.com>
2019-10-31 20:21:29 +0600

    Full Log in app

--- login_app/lib/ui/CustomLogIn.dart
+++ login_app/lib/ui/CustomLogIn.dart
@@ -6,8 +6,60 @@ class CustomLogIn extends StatefulWidget {
 }
 
 class _CustomLogInState extends State<CustomLogIn> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return new Text("Hello World");
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(child: new Image.asset("images/login.png",width: 100,height: 100,),),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField (
+              decoration: InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(3),
+                  gapPadding: 3
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "PassWord",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(3),gapPadding: 3)
+              ),
+            ),
+          ),
+          Container(
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: RaisedButton(color:Colors.white,onPressed: (){},
+                         child:new Text("Submit") ,),
+
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(color:Colors.white,onPressed: (){
+                  },
+                    child:new Text("Clear") ,),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
